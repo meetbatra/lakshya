b16426237f4ea5c8b565573f52fb9035ac14368a
Meet Batra <meetbatra56@gmail.com>
2025-09-22 13:08:17 +0530

    Add Vercel frontend URL to CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://lakshya-six.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
